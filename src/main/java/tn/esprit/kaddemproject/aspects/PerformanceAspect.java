8d31c80fa41728a1923d9fbbaad34b69e01157a4
Houssem Balti <86389183+houssembalti@users.noreply.github.com>
2023-11-05 18:02:01 +0100

    Update PerformanceAspect.java

--- src/main/java/tn/esprit/kaddemproject/aspects/PerformanceAspect.java
+++ src/main/java/tn/esprit/kaddemproject/aspects/PerformanceAspect.java
@@ -29,7 +29,7 @@ public class PerformanceAspect {
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-//	    List<Object> obj = new ArrayList<Object>(); // si le type de retour est une listeeeee
+//	    List<Object> obj = new ArrayList<Object>(); // si le type de retour est une listeeeeee
 //		obj = (List<Object>) pjp.proceed();:
 		Object obj = pjp.proceed();
 
