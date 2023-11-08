ee8d060442a85cc86d473bba04203c8a90294df3
chehinedh <chehine.dhemaied@esprit.tn>
2023-11-08 17:33:39 +0100

    update jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,17 +3,17 @@ pipeline {
         label 'agent-ubuntu1'
     }
     triggers {
-       githubPush()
+        githubPush()
     }
 
     stages {
         stage('Checkout') {
             steps {
-                git branch:'chehine', credentialsId: 'github_chehine', url: 'https://github.com/Devops5twin2/DevopsSpring'
+                git branch: 'chehine', credentialsId: 'github_chehine', url: 'https://github.com/Devops5twin2/DevopsSpring'
             }
         }
-        
-        stage('Start Nexus and SQL') {
+
+        stage('Start MySQL') {
             steps {
                 script {
                     // Start only the Nexus and SQL services
@@ -21,24 +21,37 @@ pipeline {
                 }
             }
         }
-        
+
+        stage('SonarQube Analysis') {
+            steps {
+                sh "./mvnw clean verify sonar:sonar -Dsonar.projectKey=devops-kaddem -Dsonar.projectName='devops-kaddem' -Dsonar.login=29dfe77363d51f8ecd848f50149766704abce9cb"
+            }
+        }
+
         stage('Maven Build and Deploy to Nexus') {
             steps {
                 sh 'chmod +x mvnw'
-                sh './mvnw clean deploy -DskipTests '
+                sh './mvnw clean deploy -Dspring.profiles.active=prod'
             }
         }
-        
+
         stage('Build and Start Spring Application') {
             steps {
                 script {
                     // Now build and start the Spring application container
-                    sh 'docker-compose -f docker-compose.yml up -d my-spring-app'
+                    sh 'sudo docker-compose -f docker-compose.yml up -d my-spring-app'
                 }
             }
         }
+
+        // stage('Prometheus & Grafana Jenkins supervision') {
+        //     steps {
+        //         sh 'docker start prometheus'
+        //         sh 'docker start grafana'
+        //     }
+        // }
     }
-    
+
     post {
         success {
             echo 'Pipeline executed successfully'
---
+++ src/test/java/tn/esprit/kaddemproject/services/ContratServiceImpTest.java
@@ -0,0 +1,129 @@
+package tn.esprit.kaddemproject.services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.kaddemproject.entities.Contrat;
+import tn.esprit.kaddemproject.entities.Etudiant;
+import tn.esprit.kaddemproject.repositories.ContratRepository;
+import tn.esprit.kaddemproject.repositories.EtudiantRepository;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+public class ContratServiceImpTest {
+    @InjectMocks
+    private IContratServiceImp contratService;
+
+    @Mock
+    private ContratRepository contratRepository;
+    @Mock
+    private EtudiantRepository etudiantRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    public void testAddContrat() {
+        Contrat mockContrat = new Contrat();
+        when(contratRepository.save(any(Contrat.class))).thenReturn(mockContrat);
+
+        Contrat contrat = contratRepository.save(new Contrat());
+
+        assertNotNull(contrat);
+        verify(contratRepository, times(1)).save(any(Contrat.class));
+    }
+
+    @Test
+    public void testRetrieveAllContrats() {
+        List<Contrat> mockContrats = Arrays.asList(new Contrat(), new Contrat());
+        when(contratRepository.findAll()).thenReturn(mockContrats);
+
+        List<Contrat> contrats = contratRepository.findAll();
+
+        assertEquals(mockContrats.size(), contrats.size());
+        verify(contratRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testRetrieveContrat() {
+        Contrat mockContrat = new Contrat();
+        when(contratRepository.findById(anyInt())).thenReturn(Optional.of(mockContrat));
+
+        Contrat contrat = contratRepository.findById(1).orElse(null);
+
+        assertNotNull(contrat);
+        verify(contratRepository, times(1)).findById(anyInt());
+    }
+    @Test
+    public void testAffectContratToEtudiant() {
+        // Arrange
+        Contrat mockContrat = new Contrat();
+        Etudiant mockEtudiant = new Etudiant();
+        when(contratRepository.getReferenceById(anyInt())).thenReturn(mockContrat);
+        when(etudiantRepository.findByNomEAndPrenomE(anyString(), anyString())).thenReturn(mockEtudiant);
+
+        // Act
+        Contrat result = contratService.affectContratToEtudiant(1, "John", "Doe");
+
+        // Assert
+        verify(contratRepository, times(1)).getReferenceById(1);
+        verify(etudiantRepository, times(1)).findByNomEAndPrenomE("John", "Doe");
+        assertEquals(mockEtudiant, mockContrat.getEtudiant());
+        assertEquals(mockContrat, result);
+    }
+
+    @Test
+    public void testNbContratsValides() {
+        // Arrange
+        LocalDate startDate = LocalDate.of(2023, 1, 1);
+        LocalDate endDate = LocalDate.of(2023, 12, 31);
+        when(contratRepository.countByArchiveIsFalseAndDateDebutContratAfterAndDateFinContratBefore(startDate, endDate)).thenReturn(5);
+
+        // Act
+        Integer result = contratService.nbContratsValides(startDate, endDate);
+
+        // Assert
+        verify(contratRepository, times(1)).countByArchiveIsFalseAndDateDebutContratAfterAndDateFinContratBefore(startDate, endDate);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testRetrieveAndUpdateStatusContrat() {
+        // Arrange
+        LocalDate currentDate = LocalDate.now();
+        List<Contrat> mockContrats = new ArrayList<>();
+        when(contratRepository.findByArchiveIsFalse()).thenReturn(mockContrats);
+
+        // Act
+        contratService.retrieveAndUpdateStatusContrat();
+
+        // Assert
+        verify(contratRepository, times(1)).findByArchiveIsFalse();
+
+    }
+
+    @Test
+    public void testArchiveExpiredContracts() {
+        // Arrange
+        LocalDate currentDate = LocalDate.now();
+        List<Contrat> mockExpiredContrats = new ArrayList<>();
+        when(contratRepository.findByArchiveIsFalseAndDateFinContrat(currentDate)).thenReturn(mockExpiredContrats);
+
+        // Act
+        contratService.archiveExpiredContracts();
+
+        // Assert
+        verify(contratRepository, times(1)).findByArchiveIsFalseAndDateFinContrat(currentDate);
+    }
+
+}
+
